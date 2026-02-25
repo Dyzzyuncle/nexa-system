48940abfe7c831121a9e0ac0c904959b241c3c02
Dyzzyuncle <pettinarigianni565@gmail.com>
2026-02-25 19:16:25 -0300

    Update datos.js

--- datos.js
+++ datos.js
@@ -1,5 +1,15 @@
+// ===============================
+// BASE DE DATOS DE ALUMNOS
+// ===============================
+
 const alumnos = {
-  "NEXA-001-CFPM": "plus",
-  "NEXA-002-CFPM": "base",
-  "NEXA-003-CFPM": "nopago"
+    "NEXA-001-CFPM": { plan:"plus", vence:"2026-12-31" },
+    "NEXA-002-CFPM": { plan:"base", vence:"2026-03-01" },
+    "NEXA-003-CFPM": { plan:"nopago", vence:"2026-01-15" }
 };
+
+// ===============================
+// CONFIGURACIÓN ADMIN
+// ===============================
+
+const ADMIN_CODE = "ADMIN-NEXA-ROOT";
